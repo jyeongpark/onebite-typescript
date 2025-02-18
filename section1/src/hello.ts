67d18e122bf662a9bcd495cb1f8fa852f1b5eeee
jyeongpark <jiyeongstar@gmail.com>
2025-02-18 14:33:23 +0900

    section1 : 타입스크립트 컴파일러 옵션 설정

---
+++ section1/src/hello.ts
@@ -0,0 +1,5 @@
+const a = 1;
+
+export const hello = (message: string) => {
+  console.log("hello " + message);
+};
---
+++ section1/src/index.ts
@@ -0,0 +1,5 @@
+import { hello } from "./hello";
+
+const a = 1;
+
+console.log(hello("world"));
