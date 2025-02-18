79ff5392b54c0f307dd97fd3b1fbdded660cc25b
jyeongpark <jiyeongstar@gmail.com>
2025-02-18 14:46:49 +0900

    section2 : 원시타입과 리터럴타입

---
+++ section2/src/chapter1.ts
@@ -0,0 +1,27 @@
+// number
+let num1: number = 123; // 타입 주석 (annotation)
+let num2: number = -123;
+let num3: number = 0.123;
+let num5: number = Infinity;
+let num6: number = -Infinity;
+let num7: number = NaN;
+
+// string
+let str1: string = "hello";
+let str2: string = `hello`;
+let str3: string = `hello ${num1}`;
+
+// boolean
+let bool1: boolean = true;
+let bool2: boolean = false;
+
+// null
+let nul1: null = null;
+
+// undefined
+let unde1: undefined = undefined;
+
+// let numA: number = null; // 임시로 null 넣기 tsconfig.json -> "strictNullChecks": false
+
+// 리터럴 타입, 값 그 자체가 타입
+let numA: 10 = 10;
